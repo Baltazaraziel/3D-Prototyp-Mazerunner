4039e3180bf89d86b101553984a1adbc17f489b3
Baltazaraziel/Philip <constructor.1995@freenet.de>
2017-04-17 22:37:59 +0200

    Player Input (Keyboard & X360 controller) and Movement
    Added UpdateInput method to register keyboard and controller input.
    Scaled the model to better fit the map.

--- 3DPrototypMazeRunner/3DPrototypMazeRunner/Player.cs
+++ 3DPrototypMazeRunner/3DPrototypMazeRunner/Player.cs
@@ -8,18 +8,20 @@ namespace _3DPrototypMazeRunner
 {
     class Player
     {
-        Model playerModel;
+        Model pModel;
         // Set the position of the model in world space, and set the rotation.
-        public Vector3 playerPosition;
-        public float playerRotation = 0.0f;
-        private float scale = 0.1f;
+        public Vector3 pPosition;
+        public float pRotation = 0.0f;
+        private float scale = 0.025f;
+
+        public Vector3 pVelocity = Vector3.Zero;
 
         /// <summary>
         /// Constructor
         /// </summary>
         public Player(Vector3 StartPos)
         {
-            playerPosition = StartPos;
+            pPosition = StartPos;
         }
 
         /// <summary>
@@ -29,23 +31,31 @@ namespace _3DPrototypMazeRunner
         /// <param Graphicsdevice object="device"></param>
         public void Initialize(ContentManager contentManager, GraphicsDevice device)
         {
-            playerModel = contentManager.Load<Model>("Models/Player");
+            pModel = contentManager.Load<Model>("Models/Player");
             //modelPosition = Map.Startpos;
         }
 
         public void Update()
         {
+            
+
+            // Add velocity to the current position.
+            pPosition += pVelocity;
+
+            // Bleed off velocity over time.
+            pVelocity *= 0.95f;
+
 
         }
-
+       
         public void Draw(Matrix projection, Matrix view, Matrix world)
         {
             // Copy any parent transforms.
-            Matrix[] transforms = new Matrix[playerModel.Bones.Count];
-            playerModel.CopyAbsoluteBoneTransformsTo(transforms);
+            Matrix[] transforms = new Matrix[pModel.Bones.Count];
+            pModel.CopyAbsoluteBoneTransformsTo(transforms);
 
             // Draw the model. A model can have multiple meshes, so loop.
-            foreach (ModelMesh mesh in playerModel.Meshes)
+            foreach (ModelMesh mesh in pModel.Meshes)
             {
                 // This is where the mesh orientation is set, as well 
                 // as our camera and projection.
@@ -55,8 +65,8 @@ namespace _3DPrototypMazeRunner
                     //transforms mesh by scaling then rotating and translating
                     effect.World = transforms[mesh.ParentBone.Index] 
                         * Matrix.CreateScale(scale) 
-                        * Matrix.CreateRotationY(playerRotation)
-                        * Matrix.CreateTranslation(playerPosition);
+                        * Matrix.CreateRotationY(pRotation)
+                        * Matrix.CreateTranslation(pPosition);
                     effect.View = view;
                     effect.Projection = projection;
                 }
